b1e353fba87fd776e2655a7555c172bf8829126e
Óscar Morales Vivó <oscarmv@mac.com>
2023-06-06 23:40:40 -0700

    Disable UI tests for github actions CI runs.

--- PDFPagePickerTestAppUITests/PDFPagePickerTestAppUITests.swift
+++ PDFPagePickerTestAppUITests/PDFPagePickerTestAppUITests.swift
@@ -13,12 +13,12 @@ final class PDFPagePickerTestAppUITests: XCTestCase {
         continueAfterFailure = false
     }
 
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
 }
